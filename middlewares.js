c47fe7cebdd75777b5ff2b574c2eeb7787dc8c2f
unknown <albertbenig@gmail.com>
2022-04-28 19:38:56 +0200

    use cors

--- middlewares.js
+++ middlewares.js
@@ -2,7 +2,12 @@ const authmiddleware = require('./tools/auth-middleware');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const corsOptions = { origin: "https://poke-team-frontend.herokuapp.com/" }
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
 
 const setupMiddlewares = (app) => {
     app.use(bodyParser.json());
