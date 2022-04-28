b4f1c20a9b8dd9b5406314de3877fcc9a9c14fbe
unknown <albertbenig@gmail.com>
2022-04-28 21:02:38 +0200

    use cors

--- middlewares.js
+++ middlewares.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    "optionsSuccessStatus": 204
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Credentials': true,
 }
 
 const setupMiddlewares = (app) => {
