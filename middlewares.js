e8b0cf845a6a15994a344847ac7ee3f0d5ffdcf7
unknown <albertbenig@gmail.com>
2022-04-28 12:23:28 +0200

    make it better

--- middlewares.js
+++ middlewares.js
@@ -1,11 +1,10 @@
 const authmiddleware = require('./tools/auth-middleware');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+
 
 const setupMiddlewares = (app) => {
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(cors({optionsSuccessStatus: 200, credentials: true}));   
+    app.use(bodyParser.urlencoded({extended: true}));  
     authmiddleware.init();
     app.use(authmiddleware.protectWithJwt);
 }
