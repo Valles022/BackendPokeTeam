f6839d83cd9f2e2174ab14b763d1811fc656b915
unknown <albertbenig@gmail.com>
2022-04-29 00:14:04 +0200

    use cors

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ const app = express();
 app.use('/uploads', express.static(__dirname +'/uploads'))
 middlewares.setupMiddlewares(app);
 
+app.get('/', () => {
+    res.send('Pagina principal de backend');
+});
 app.use('/auth', authRoutes);
 app.use('/teams', teamRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Server started at port ' + process.env.PORT);
-    res.send('Pagina principal de backend');
 })
 
 exports.app = app;
